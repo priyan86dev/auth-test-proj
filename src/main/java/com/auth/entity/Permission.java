f302f6d00039f1a1af512402f391ba2ad2ca226c
priyan86dev <priyan86dev@gmail.com>
2021-05-13 18:41:20 +0530

    no need

--- src/main/java/com/auth/entity/Permission.java
+++ src/main/java/com/auth/entity/Permission.java
@@ -1,47 +1,47 @@
-package com.auth.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "permissions")
-public class Permission {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int perm_id;
-
-	@Enumerated(EnumType.STRING)
-	private Permissions pname;
-
-	public Permission() {
-	}
-
-	public Permission(Permissions pname) {
-		this.pname = pname;
-	}
-
-	public int getPerm_id() {
-		return perm_id;
-	}
-
-	public void setPerm_id(int perm_id) {
-		this.perm_id = perm_id;
-	}
-
-	public Permissions getPname() {
-		return pname;
-	}
-
-	public void setPname(Permissions pname) {
-		this.pname = pname;
-	}
-
-	
-
-}
+//package com.auth.entity;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.EnumType;
+//import javax.persistence.Enumerated;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "permissions")
+//public class Permission {
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private int perm_id;
+//
+//	@Enumerated(EnumType.STRING)
+//	private Permissions pname;
+//
+//	public Permission() {
+//	}
+//
+//	public Permission(Permissions pname) {
+//		this.pname = pname;
+//	}
+//
+//	public int getPerm_id() {
+//		return perm_id;
+//	}
+//
+//	public void setPerm_id(int perm_id) {
+//		this.perm_id = perm_id;
+//	}
+//
+//	public Permissions getPname() {
+//		return pname;
+//	}
+//
+//	public void setPname(Permissions pname) {
+//		this.pname = pname;
+//	}
+//
+//	
+//
+//}
--- src/main/java/com/auth/entity/Permissions.java
+++ src/main/java/com/auth/entity/Permissions.java
@@ -1,6 +1,6 @@
-package com.auth.entity;
-
-public enum Permissions {
-
-	P_CREATE, P_READ, P_UPDATE, P_DELETE
-}
+//package com.auth.entity;
+//
+//public enum Permissions {
+//
+//	P_CREATE, P_READ, P_UPDATE, P_DELETE
+//}
